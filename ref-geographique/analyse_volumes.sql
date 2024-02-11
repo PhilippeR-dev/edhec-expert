2863a679c6ca22d7c1df4a3f1d23b4206ab073c0
Philippe REY <prey@synergy.fr>
2024-02-12 00:21:24 +0100

    modif présentation

--- ref-geographique/analyse_volumes.sql
+++ ref-geographique/analyse_volumes.sql
@@ -1,5 +1,12 @@
 select count(1) from PLACES;
 -- 3 858 264
+
+select count(1)
+from REF_DEV.REF_GEOGRAPHIQUE.PLACES PL
+     inner join REF_DEV.REF_GEOGRAPHIQUE.DATA_LANGUAGES DL on DL.ISO = PL.ISO
+;
+-- 3 858 264
+
 select count(1) from PLACES_DENORM;
 -- 15 787 373
 
@@ -37,3 +44,23 @@ order by 2 desc;
 
 
 SELECT count(1) FROM REF_DEV.PUBLIC.PLACES_POSTCODES_SEARCH;
+-- 15 492 637
+
+
+select * from REF_DEV.REF_GEOGRAPHIQUE.DATA_LANGUAGES
+order by iso;
+
+
+select * from REF_DEV.REF_GEOGRAPHIQUE.DATA_LANGUAGES
+where iso = 'GB';
+
+
+select count(1) from REF_DEV.REF_GEOGRAPHIQUE.DATA_LANGUAGES;
+-- 247
+
+
+select tb.COUNTRY_CODE, tb.PLACE_ID, tb.LOCALITY, count(1)
+from REF_DEV.PUBLIC.COUNTRY_PLACE_POSTCODES_SEARCH tb
+group by tb.COUNTRY_CODE, tb.PLACE_ID, tb.LOCALITY 
+order by 4 desc
+; 
